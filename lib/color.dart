7aff6a017ca62e1294b14bd7779cabde4e144d04
toan722003 <nguyennhutoan722003@gmail.com>
2022-11-07 11:57:40 +0700

    Start New Project

---
+++ lib/color.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UIColor {
+  static Color colorWord = const Color(0xFF000000);
+  static Color colorBackground = const Color(0xFFFFFFFF);
+  static Color btOnOff = const Color(0xFFF4F5F6);
+  static Color bgClockItem = const Color(0xFFF4F5F6);
+  static Color btNonSelected = const Color(0xFF000000).withOpacity(0.58);
+  static Color btDayInWeek = const Color(0xFFC5C7C9);
+  static Color accentGreen = const Color(0xFF31DC43);
+  static Color accentRed = const Color(0xFFF06674);
+  static Color linear = const Color(0xFF3690EA);
+  static Color red  = const Color(0xFFFF364B);
+  static Color selectedDays  = const Color(0xFF52DC21);
+  static Color circlePercent  = const Color(0xFFF1515B);
+  static Color textPercent  = const Color(0xFF4E5964);
+  static Color startTimer  = const Color(0xFF20CC11);
+  static Color yellow  = const Color(0xFFEBFF00);
+  static Color green  = const Color(0xFF0CB109);
+  static Color createTask  = const Color(0xFF13F945);
+  static Color accentBlue  = const Color(0xFF18F6E9);
+  static Color noteColor1  = const Color(0xFF18F62E);
+  static Color noteColor2  = const Color(0xFF36B7FF);
+  static Color noteColor3  = const Color(0xFF36FFF3);
+  static Color noteColor4  = const Color(0xFFBF36FF);
+}
