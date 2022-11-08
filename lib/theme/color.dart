e9303d458755faf604a90c6a7797e89a6fc4c676
toan722003 <nguyennhutoan722003@gmail.com>
2022-11-08 19:37:16 +0700

    splashscreen

---
+++ lib/theme/color.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UIColor {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color btOnOff = Color(0xFFF4F5F6);
+  static Color btNonSelected = Color(0xFF000000).withOpacity(0.58);
+  static const Color accentGreen = Color(0xFF31DC43);
+  static const Color accentRed = Color(0xFFF06674);
+  static const Color linear = Color(0xFF3690EA);
+  static const Color red = Color(0xFFFF364B);
+  static const Color selectedDays = Color(0xFF52DC21);
+  static const Color circlePercent = Color(0xFFF1515B);
+  static const Color startTimer = Color(0xFF20CC11);
+  static const Color yellow = Color(0xFFEBFF00);
+  static const Color green = Color(0xFF0CB109);
+  static const Color createTask = Color(0xFF13F945);
+  static const Color accentBlue = Color(0xFF18F6E9);
+  static const Color noteColor1 = Color(0xFF18F62E);
+  static const Color noteColor2 = Color(0xFF36B7FF);
+  static const Color noteColor3 = Color(0xFF36FFF3);
+  static const Color noteColor4 = Color(0xFFBF36FF);
+  static const Color accentWhile = Color(0xFFC5C7C9);
+  static const Color accentGrey = Color(0xFF4E5964);
+  static const Color accnetWhite = Color(0xFFD9D9D9);
+}
---
+++ lib/theme/textStyle.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'color.dart';
+
+class UITextStyle {
+  static TextStyle item_clock_black_40_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 40, fontWeight: FontWeight.bold),
+  );
+  static TextStyle item_clock_black_15_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 15, fontWeight: FontWeight.bold),
+  );
+  static TextStyle button_select_black_16_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 16, fontWeight: FontWeight.bold),
+  );
+  static TextStyle button_select_accentWhite_16_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.accentWhile, fontSize: 16, fontWeight: FontWeight.bold),
+  );
+  static TextStyle function_clock_black_15_normal = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 15, fontWeight: FontWeight.normal),
+  );
+  static TextStyle taskbar_item_black_12_normal = GoogleFonts.openSans(
+    textStyle: TextStyle(color: UIColor.black.withOpacity(0.58), fontSize: 12, fontWeight: FontWeight.normal),
+  );
+  static TextStyle taskbar_item_black_12_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 12, fontWeight: FontWeight.bold),
+  );
+  static TextStyle cancel_black_24_normal = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 24, fontWeight: FontWeight.normal),
+  );
+  static TextStyle save_white_24_normal = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.white, fontSize: 24, fontWeight: FontWeight.normal),
+  );
+  static TextStyle day_white_13_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.white, fontSize: 13, fontWeight: FontWeight.bold),
+  );
+  static TextStyle text_notifical_red_20_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.red, fontSize: 20, fontWeight: FontWeight.bold),
+  );
+  static TextStyle percent_black_25_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 25, fontWeight: FontWeight.bold),
+  );
+  static TextStyle textViewPeecent_black_18_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 18, fontWeight: FontWeight.bold),
+  );
+  static TextStyle countTask_accantGrey_14_normal = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.accentGrey, fontSize: 14, fontWeight: FontWeight.normal),
+  );
+  static TextStyle todayTask_black_20_bold = text_notifical_red_20_bold.copyWith(color: UIColor.black);
+  static TextStyle newTask_black_24_bold = cancel_black_24_normal.copyWith(fontWeight: FontWeight.bold);
+  static TextStyle button_newTask_white_20_bold = text_notifical_red_20_bold.copyWith(color: UIColor.white.withOpacity(0.88));
+  static TextStyle hide_text_accentWhite_12_normal = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.accnetWhite, fontSize: 12, fontWeight: FontWeight.normal),
+  );
+  static TextStyle time_black_70_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.black, fontSize: 70, fontWeight: FontWeight.bold),
+  );
+  static TextStyle text_notifical_red_28_bold = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.red, fontSize: 28, fontWeight: FontWeight.bold),
+  );
+  static TextStyle note_white_12_normal = GoogleFonts.openSans(
+    textStyle: const TextStyle(color: UIColor.white, fontSize: 12, fontWeight: FontWeight.normal),
+  );
+  static TextStyle note_white_16_bold = GoogleFonts.openSans(
+    textStyle: TextStyle(color: UIColor.white, fontSize: 16, fontWeight: FontWeight.bold),
+  );
+}
